bc73f7878ea686acb0a41bfff82031d4a2c9a866
igor4022 <tic80padraolua@gmail.com>
2025-12-21 20:39:45 -0300

    blz

---
+++ Variaveis/script.js
@@ -0,0 +1,30 @@
+// console.log('Ola MUndo!')
+
+// Exemplo 1:
+
+var nome = 'Igor'
+var idade = 28
+var pos = true
+
+console.log(nome, idade, pos, nome)
+
+// Exemplo 2:
+
+var preso = 25
+var total = 5
+var talpres  = total + preso
+
+console.log(talpres)
+
+// Exemplo 3:
+
+var sobnome = 'Maciel',
+    cidade = 'Timbó'
+
+console.log(sobnome, cidade)
+
+// Exemplo 4:
+
+var notdifine
+
+console.log(notdifine)

4c66de1ce912db14c4bcfd2cc4844138b752acad
igor4022 <tic80padraolua@gmail.com>
2025-12-21 20:44:05 -0300

    sim

---
+++ Variaveis/Exercicio/script.js
@@ -0,0 +1,22 @@
+// Declarar uma variável com seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com sua comida
+// favorita e não atribuir var
+
+// Atribuir valor a sua comida favorita
+
+// Declarar 5 variáveis diferentes sem valores
+
+var nome = 'Igor'
+var idade = 16
+let comida = 'Pizza'
+var favorita = 7
+var variavel
+var sem
+var nenhum
+var valor
+var mesmo
+
+console.log(nome, cidade, comida, favorita)

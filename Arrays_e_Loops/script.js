bfa6bdd9180734368f91cf0f222c879721c2194e
igor4022 <tic80padraolua@gmail.com>
2025-12-25 16:27:16 -0300

    sim

---
+++ Arrays_e_Loops/script.js
@@ -0,0 +1,17 @@
+// Exemplo 1:
+
+// Array é um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável...
+
+// o metodo 'pop()' remove o último item e retorna ele...
+
+// o metodo 'push()' adicina um array no final da lista...
+
+var videoGames = ['Switch', 'PS4', 'XBox']
+
+var item = videoGames.pop()
+
+videoGames.push('3DS')
+
+// Exemplo 2:
+
+// For Loop fazem algo repitidamente até que uma condição seja atingida...

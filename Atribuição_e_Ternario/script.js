028e33ebbf221955aca9f8263083c4390c837cfe
igor4022 <tic80padraolua@gmail.com>
2025-12-25 19:21:21 -0300

    sim

---
+++ Atribuição_e_Ternario/script.js
@@ -0,0 +1,18 @@
+// Exemplo 1:
+
+var num  = 20
+
+num += 10
+
+console.log(num)
+
+// Exemplo 2:
+
+// Operador Ternário...Abreviação de condicionais com 'if' e 'else'...
+
+var idade = 20
+var beber
+
+beber = (idade >= 18) ? 'Pode beber' : 'Não pode beber'
+
+console.log(beber)

a2a03489b942503a44307e86a6589e7e47e62c7b
igor4022 <tic80padraolua@gmail.com>
2025-12-29 20:24:08 -0300

    sim

--- Selecionar_o_DOM/script.js
+++ Selecionar_o_DOM/script.js
@@ -32,10 +32,26 @@ console.log(primeiraUl)
 
 const linksInternos = document.querySelector('[href^="#"]')
 
-console.log(linksInternos.href)
+console.log(linksInternos)
 
 // Exemplo 4:
 
 // Seletor Geral Lista...'guerySelectorAll' retorna todos os elementos compatíveis com o seletor CSS em uma NodeList...
 
-const animaisImg
+const animaisImg = document.querySelectorAll('.animais img')
+
+console.log(animaisImg)
+
+// Exemplo 5:
+
+// HTMLAllCollection vs NodeList...
+
+// A diferença está nos métodos e propriedades de ambas. Além disso a NodeList retornada com querySelectorAll é estática...
+
+const gridSectionHTML = document.getElementsByClassName('grid-section')
+const gridSectionNode = document.querySelectorAll('.grid-section')
+
+primeiraUl
+
+console.log(gridSectionHTML)
+console.log(gridSectionNode)

e639590c4691cebb724cabbf5aaedce08260bc58
igor4022 <tic80padraolua@gmail.com>
2025-12-29 22:54:52 -0300

    blz

--- Foreach_e_Arrow_Function/script.js
+++ Foreach_e_Arrow_Function/script.js
@@ -15,11 +15,72 @@ img.forEach(function(item) {
 
 // O primeiro parâmetro é o callback, ou seja, a função que será ativada a cada item. Essa função pode receber três parâmetros: ValorAtual, index e array...
 
-const imgs = document.querySelector('img')
+const imgs = document.querySelectorAll('img')
 
-imgs.forEach(function(ValorAtual, index, array) {
+imgs.forEach(function(item, index, array) {
 
     console.log(item)
     console.log(index)
     console.log(array)
 });
+
+// Exemplo 3:
+
+const imagens = document.querySelectorAll('img')
+
+let i = 0
+imagens.forEach(function(item) {
+
+    console.log(i++)
+})
+
+// Exemplo 4:
+
+// ForEach e Array...forEach é um método de Array, alguns objetos array-like possuem este métodos. Caso não possua, o ideal é transformá-los em uma array...
+
+const titulos = document.getElementsByClassName('titulo')
+const tituloArray = Array.from(titulos)
+
+tituloArray.forEach(function(item, index, array){
+
+    console.log(item, index, array)
+} )
+
+// Exemplo 5:
+
+// Arrow Function...Sintaxe curta em relação a 'function ezxpression'. Basta remover a palavra chave function e adicionar a fat arrow '=>' após os argumentos...
+
+const imagem = document.querySelectorAll('img')
+
+imagem.forEach((item, index) => {
+
+    console.log(item, index)
+})
+
+// Se você so tiver um argumento você não precisa colocar '()'...
+
+const imagem2 = document.querySelectorAll('img')
+
+imagem2.forEach(item => {
+
+    console.log(item)
+})
+
+// Porem se não tiver nem um argumento você necessita de colocar '()'...
+
+const imagem3 = document.querySelectorAll('img')
+
+let val = 0
+
+imagem3.forEach(() => {
+
+    console.log(val++)
+})
+
+// Sé o seu código so tiver uma linha você pode simplificalo tirando as '{}'...
+
+const img2 = document.querySelectorAll('img')
+
+let val2 = 0
+
+img2.forEach(() => console.log(i++))

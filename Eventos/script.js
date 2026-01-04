bdc84b641181f9139c6409f7e274a50d08e65f0e
igor4022 <tic80padraolua@gmail.com>
2026-01-04 20:53:34 -0300

    sim

---
+++ Eventos/script.js
@@ -0,0 +1,5 @@
+// Exemplo 1:
+
+// Adiciona uma função ao elemento, esta chamada 'callback'. Que será atividade assim que certo'evento' ocorrer neste elemento...
+
+const img = document.querySelector('img')

fedc44595e0f2c1a1bbac27414d1f86eda966772
igor4022 <tic80padraolua@gmail.com>
2026-01-11 00:35:59 -0300

    sim

---
+++ OBJETOS/script.js
@@ -0,0 +1,13 @@
+const carro = {
+    marca: 'Marca',
+    preso: 0,
+}
+
+const honda = carro
+
+honda.marca = 'Honda'
+honda.preso = 2000
+
+const fiat = carro
+fiat.marca = 'fiat'
+fiat.preso = 3000

a1a7adecf881950f3365e06f8b1e07c15d6e9570
igor4022 <tic80padraolua@gmail.com>
2026-01-12 20:18:26 -0300

    sim

---
+++ OBJETOS/Native-host-e-user/Exercícios/script.js
@@ -0,0 +1,21 @@
+// Liste 5 objetos nativos
+
+Object
+String
+Array
+Number
+Function
+
+// Liste 5 objetos do browser
+
+window
+document
+onclick
+alert
+top
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+event.keyCode 
+event.whic
---
+++ OBJETOS/Native-host-e-user/script.js
@@ -0,0 +1,28 @@
+// Exemplo 1:
+
+// Native...Objetos nativos são aqueles definidos na especificação da linguagem e são implementados independente do host...
+
+// Exemplo 2:
+
+// User...Objetos do user, são os objetos definidos pelo seu aplicativo. Ou seja, qualquer objeto que você criar ou que importar de alguma biblioteca externa...
+
+// Exemplo 3:
+
+// Bibliotecas...Bibliotecas como jQuery foram criadas para resover o problema de inconsistências entre browsers e adicionar funcionalidades que não existiam nativamente...
+
+// A padronização dos browsers e a implementação de soluções nativas, torna as mesmas obsoletas...
+
+// Exemplo 4:
+
+// Verificar se Existe...O typeof retorna o tipo de dado. Caso esse dado não existe ou não exista ou não tenha sido definido, ele irá reotrnar undefined. Ou seja, quando não for undefined que dizer que existe...
+
+console.log(typeof Array.from !== "undefined")
+
+
+if (typeof Array.ffsfsf !== "undefined") {
+
+    console.log('Existe')
+} else {
+    
+    console.log('Não existe')
+}

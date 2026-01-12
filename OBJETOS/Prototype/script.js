3eccf85d355c19784a8d8f404080d1877752b05b
igor4022 <tic80padraolua@gmail.com>
2026-01-12 17:22:45 -0300

    sim

--- OBJETOS/Prototype/script.js
+++ OBJETOS/Prototype/script.js
@@ -7,7 +7,7 @@ function Pessoa(nome, idade) {
     this.idade = idade
 }
 
-const andere = new Pessoa('Andre', 28)
+const andere = new Pessoa('Andre ', 28)
 
 console.log(Pessoa.prototype)
 
@@ -16,5 +16,57 @@ console.log(Pessoa.prototype)
 // É possível adicionar novas propriedades e métodos ao objeto prototype...
 
 Pessoa.prototype.andar = function() {
-    return 'Pessoa andou'
+
+    return this.nome + 'Pessoa andou'
+}
+
+Pessoa.prototype.nadar = function() {
+
+    return this.nome + 'Pessoa nadou'
+}
+
+// Acesso ao Protótipo...O objeto criado utilizando o construtor. Lembrando, prototype é uma propriedade e funções apenas...
+
+// Exemplo 3:
+
+// Construtores Nativos...Objetos, Funções, Números, Strings e outros tipos de dados são criados utilizando construtores...
+
+// Esses construtores possuem um protótipo com propriedades e métodos, que poderão ser acessadas pelo tipo de dado...
+
+const pais = 'Brasil'
+const cidade = new String('Rio')
+
+// Exemplo 4:
+
+// É possível acessar a função do protótipo...
+
+// É comum, principalmente em códigos mais antigos, o uso direto de funções do protótipo do construtor Array...
+
+const listaAnimais = ['Cachorro', 'Gato', 'Coruja']
+
+const lista = document.querySelectorAll('li')
+
+const listaArray = Array.prototype.slice.call(lista)
+
+// Exemplo 5:
+
+// Métodos do Objeto vs Protótipo...
+
+// Nos objetos nativos existem métodos linkados diretamente ao Objeto e outros linkados ao protótipo...
+
+const listaArray1 = Array.prototype.slice.call(lista)
+const listaArray2 = Array.from(lista)
+
+// Exemplo 6:
+
+// Entenda o Que está Sendo Retornado.....
+
+// Os métodos e propriedades acessado com o '.' são referentes ao tipo de dados que é retornado antes desse '.'...
+
+const Carro = {
+    marca: 'Ford',
+    preco: 2000,
+    andar() {
+        return true
+    }
 }

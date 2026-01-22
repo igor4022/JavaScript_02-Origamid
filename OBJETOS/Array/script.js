9c562484a02e82f17027365115c1e033a2585ec2
igor4022 <tic80padraolua@gmail.com>
2026-01-22 11:10:06 -0300

    sim

--- OBJETOS/Array/script.js
+++ OBJETOS/Array/script.js
@@ -88,3 +88,67 @@ console.log(carros.shift())
 // [].reverse() inverte os itens da array e retorna a nova array...
 
 console.log(carros.reverse())
+
+// EXemplo 11:
+
+// []splice()...[].splice(index, item1, item2, ...)adiciona valores na array a partir do index, Remove a quantidade de itens que for passada no segundo parâmetro(retorna esse itens)...
+
+carros.splice(2, 0, 'Fusca')
+
+console.log(carros)
+
+// Exemplo 12:
+
+// [].copyWithin(alvo, inicio, final) a partir do alvo, ele irá copiar a array começando do inicio até o final e vai preencher a mesma com essa cópia. Caso omita os valores de ínicio e final, ele irá utilizar como inicio o 0 e o final o valor total da array...
+
+console.log(['item1', 'item2', 'item3', 'item4', 'item5'].copyWithin(2, 0, 3))
+
+// Exemplo 13:
+
+// [].fill()...[].fill(valor, inicio, final) prenche a array com o valor, do ínicio até o fim...
+
+console.log(['item01', 'item02', 'item03', 'item04', 'item05'].fill('Café'))
+
+console.log(['item01', 'item02', 'item03', 'item04', 'item05'].fill('Café', 1))
+
+console.log(['item01', 'item02', 'item03', 'item04', 'item05'].fill('Café', 1, 3))
+
+// Exemplo 14:
+
+// Métodos de Acesso [].concat()...
+// Os métodos abaixo não modificam a array original, apenas retornam uma array modificada. '[].concat()' irá concatenar a array com o valor passado...
+
+const transporte1 = ['Barco', 'Aviao']
+const transporte2 = ['Carro', 'Moto']
+const transportes = transporte1.concat(transporte2)
+const maisTransportes = [].concat(transporte1, transporte2)
+
+console.log(transportes)
+console.log(maisTransportes)
+
+// Exemplo 15:
+
+// [].includes(), [].indexOf() e [].lastIndexOf()...
+// '[].includes(valor)' verifica se um array possui o valor e retorna verdadeiro ou falso. '[].indexOf(valor)' verifica se um array possui o valor e retorna o índice do primeiro valor no array. Já o '[].lastIndexOf(valor)' retorna o índice do último...
+
+const linguagnes = ['html', 'css', 'js', 'php', 'pyhon', 'js']
+
+linguagnes.includes('css')
+linguagnes.indexOf('python')
+linguagnes.lastIndexOf('js')
+
+// Exemplo 16:
+
+// [].join(separador)junta todos os valores do array e retorna uma string com eles. Se você passar um valor como parâmetro, ele será utilizado durante o uso de cada item do array...
+
+let htmlString = '<h2>Título Principal</h2>'
+htmlString = htmlString.split('h2')
+htmlString = htmlString.join('h1')
+
+console.log(htmlString)
+
+// Exemplo 17:
+
+// [].slice(inicio, final)retorna os itens do array começando pelo início e indo até o valor de final...
+
+console.log(linguagnes.slice(1, 3))

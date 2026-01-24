315bd115e92effa2a4b2e304b777fe2f64fdede0
igor4022 <tic80padraolua@gmail.com>
2026-01-24 14:39:20 -0300

    blz

--- OBJETOS/Array_e_Iteração/Exercícios/script.js
+++ OBJETOS/Array_e_Iteração/Exercícios/script.js
@@ -2,16 +2,49 @@
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
 
+const cursos = [
+  {
+    nome: 'HTML 1',
+    descrisao: 'Curso básico de HTML',
+    aulas: 'Aulas de HTML básico',
+    hora: '40 horas'
+  },
+  {
+    nome: 'CSS 1',
+    descrisao: 'Curso básico de CSS',
+    aulas: 'Aulas de CSS básico',
+    horas: '40 horas'
+  },
+  {
+    nome: 'JavaScript 1',
+    descrisao: 'Curso básico de JavaScript',
+    aulas: 'Aulas de JavaScript básico',
+    horas: '40 horas'
+  }
+]
+
+cursos.map((curso) => {
+  return curso
+})
+
+console.log(cursos)
+
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
 
+const resul = numeros.filter((ns) => ns >= 100)
+
+console.log(resul)
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
 
+const confirmarBaixo = instrumentos.some((item) => item === 'Baixo')
+
+console.log(confirmarBaixo)
 
 // Retorne o valor total das compras
 const compras = [
@@ -36,3 +69,10 @@ const compras = [
     preco: 'R$ 10,60'
   }
 ]
+
+const somar = compras.reduce((acumulador, soma) => {
+  const valorFinal = +soma.preco.replace('R$','').replace(',', '.')
+  return acumulador + valorFinal
+}, 0)
+
+console.log(`R$${somar}`)

ec9b9eb98bb7f45b3ff83b7607c9c7990a0915bb
igor4022 <tic80padraolua@gmail.com>
2026-01-24 11:39:04 -0300

    sim

--- OBJETOS/Array_e_Iteração/Exercícios/script.js
+++ OBJETOS/Array_e_Iteração/Exercícios/script.js
@@ -1,50 +1,38 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
-// Remova o primeiro valor de comidas e coloque em uma variável
-// Remova o último valor de comidas e coloque em uma variável
-// Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
-comidas.shift()
-comidas.pop()
-
-const comidasMais = ['Peixe', 'Batata']
-const comida = ['Arroz']
-
-resul = comidasMais.concat(comidas, comida)
-
-console.log(resul)
-
-const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// Arrume os estudantes em ordem alfabética
-// Inverta a ordem dos estudantes
-// Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
-
-estudantes.sort()
-estudantes.reverse()
-
-console.log(estudantes.includes('Joana'))
-console.log(estudantes.includes('Juliana'))
-
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`
-// Substitua section por ul e div com li,
-// utilizando split e join
-
-html = html.split('section', 'div')
-html = html.join('ul', 'li')
-
-console.log(html)
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
 
-carrosCopia = carros.slice()
-carros.pop()
 
-console.log(carros)
-console.log(carrosCopia)
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]

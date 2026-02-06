1343281d3492207ac56cea5b76b024f27ab7a44b
igor4022 <tic80padraolua@gmail.com>
2026-02-05 23:51:28 -0300

    sim

--- efeitos-no-dom/modal/js/modules/modal.js
+++ efeitos-no-dom/modal/js/modules/modal.js
@@ -5,18 +5,24 @@ export default function initModal() {
     const btnFechar = document.querySelector('[data-modal="fechar"]')
     const containerModal = document.querySelector('[data-modal="container"]')
 
+    if(btnAbrir && btnFechar && containerModal) {
 
-    function abrirModal(event) {
-       event.preventDefault()
-       containerModal.classList.add('ativo')
+        function abrirModal(event) {
+        event.preventDefault()
+        containerModal.classList.add('ativo')
+        }
+
+        function fecharModal() {
+            containerModal.classList.remove('ativo')
+        }
+
+        function fecharForModal(event) {
+            if(event.target === this) {
+                fecharModal(this)
+            }
+        }
+        btnAbrir.addEventListener('click', abrirModal)
+        btnFechar.addEventListener('click', fecharModal)
+        containerModal.addEventListener('click', fecharForModal)
     }
-
-    btnAbrir.addEventListener('click', abrirModal)
-
-    function fecharModal() {
-        containerModal.classList.remove('ativo')
-    }
-
-    btnFechar.addEventListener('click', fecharModal)
-
 }

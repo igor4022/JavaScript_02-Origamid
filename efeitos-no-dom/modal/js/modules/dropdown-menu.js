f7326c4f245cc3d159627a6bd37a024f0888df8a
igor4022 <tic80padraolua@gmail.com>
2026-02-09 20:52:52 -0300

    sim

--- efeitos-no-dom/modal/js/modules/dropdown-menu.js
+++ efeitos-no-dom/modal/js/modules/dropdown-menu.js
@@ -4,7 +4,30 @@ export default function initDropdownMenu() {
 
     function onMouseClick(event) {
         event.preventDefault()
-        this.classList.toggle('ativo')
+        this.classList.add('ativo')
+        offMouseCLick(this, ['touchestart', 'click'], () => {
+            this.classList.remove('ativo')
+        })
+    }
+
+    function offMouseCLick(element, events, callback) {
+        const html = document.documentElement
+        const outside = 'data-outside'
+
+        if(!element.hasAttribute(outside)) {
+            events.forEach(user => {
+                html.addEventListener('click', offOutsideClick)
+            })
+            element.setAttribute(outside, '')
+        }
+
+        function offOutsideClick(event) {
+            if(!element.contains(event.target)) {
+                element.removeAttribute(outside)
+                html.removeEventListener('click', offOutsideClick)
+                callback()
+            }
+        }
     }
     
     dropDown.forEach((menu) => {

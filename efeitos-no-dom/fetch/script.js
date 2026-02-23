07dfd68c6d8f7b12cf20b12cb97a77a6eeb43769
igor4022 <tic80padraolua@gmail.com>
2026-02-23 02:52:34 -0300

    blz

--- efeitos-no-dom/fetch/script.js
+++ efeitos-no-dom/fetch/script.js
@@ -99,4 +99,77 @@ sobre.then((resolucao) => {
 
 // Exemplo 6:
 
-// 
+// .blob()...Um blob é um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transformar requisições de imagens por exemplo. O blob gera um URL único...
+
+const imagem = fetch('./Pessoa.png')
+
+imagem.then((resolucao) => {
+    return resolucao.blob()
+}).then((body) => {
+    const bloUrl = URL.createObjectURL(body)
+    const imagemDom = document.querySelector('img')
+    imagemDom.src = bloUrl
+})
+
+// Exemplo 7:
+
+// .clone()...Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o método clone, caso você precise transformar uma resposta em valores diferentes...
+
+const doc7 = fetch('https://viacep.com.br/ws/01001000/json/')
+
+doc7.then((resolucao) => {
+    const r2 = resolucao.clone()
+    resolucao.text() .then((text) => {
+        console.log(resolucao)
+    })
+    r2.json() .then((json) => {
+        console.log(json)
+    })
+}).then((body) => {
+   console.log(body)
+})
+
+// Exemplo 8:
+
+// .cabeçalhos...É uma propriedade que possui os cabeçalhos da requisição. É um tipo de dado iterável, então podemos usar o forEach para ver cada um deles...
+
+const doc8 = fetch('https://viacep.com.br/ws/01001000/json/')
+
+doc8.then((response) => {
+   console.log(response)
+   response.headers.forEach((res) => {
+    console.log(res)
+   })
+})
+
+// Exemplo 9:
+
+// .status e .ok...Retorna o status da requisição. Veja foi 404, 200, 202 e mais. ok retorna um valor booleano sendo true para uma requisição de sucesso e false para uma sem sucesso...
+
+const docs = fetch('./docs.txt')
+
+docs.then((response) => {
+   console.log(response.status)
+   
+   if(response.status === 404) {
+    console.log('Página não existe...')
+   }
+})
+
+// Exemplo 10:
+
+// .url e .tipo...urlretorna a url da requisição. .typeretorna o tipo de resposta...
+
+const div2 = document.createElement('div');
+
+const docs2 = fetch('https://viacep.com.br/ws/01001000/json/')
+
+docs2.then(response => {
+  console.log(response.type, response.url);
+});
+
+//types
+// basic: feito na mesma origem
+// cors: feito em url body pode estar disponível
+// error: erro de conexão
+// opaque: no-cors, não permite acesso de outros
